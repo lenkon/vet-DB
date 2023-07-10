ff09a0c58e0242b1aca3f1c16dcd92755098949b
Dilsher <dilsherb@gmail.com>
2023-07-10 14:16:09 +0300

    3rd query for neutered and escape attempts added.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%_mon';
+SELECT * 
+FROM animals 
+WHERE name LIKE '%_mon';
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name 
+FROM animals 
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+
+SELECT name 
+FROM animals 
+WHERE (neutered = true) and (escape_attempts<'3');
