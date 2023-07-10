e98878366f26068f7627e4d2d6416289c51b4864
Dilsher <dilsherb@gmail.com>
2023-07-10 13:58:39 +0300

    2nd query for getting data between 2 dates added.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%_mon';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
