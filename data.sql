2c0b245b4d7032bccca5c0a7dad76946660603cb
Dilsher <dilsherb@gmail.com>
2023-07-12 16:58:36 +0300

    owner_id set

--- data.sql
+++ data.sql
@@ -53,3 +53,14 @@ SET
         WHEN name LIKE '%mon' THEN 2
         ELSE 1
     END;
+
+UPDATE
+    animals
+SET
+    owner_id = CASE
+        WHEN name = 'Agumon' THEN 1
+        WHEN name IN('Gabumon','Pikachu') THEN 2
+        WHEN name IN('Devimon', 'Plantmon') THEN 3
+        WHEN name IN('Charmander', 'Squirtle', 'Blossom') THEN 4
+        WHEN name IN('Angemon', 'Boarmon') THEN 5
+    END;
