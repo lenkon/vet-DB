acac8adfb117187c1b7813b78c8f74271e41d181
Dilsher Balouch <dilsherb@gmail.com>
2023-07-12 18:29:32 +0300

    join animals & owners

--- queries.sql
+++ queries.sql
@@ -1,55 +1,34 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * 
-FROM animals 
-WHERE name LIKE '%_mon';
+SELECT * FROM animals WHERE name LIKE '%_mon';
 
-SELECT name 
-FROM animals 
-WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-SELECT name 
-FROM animals 
-WHERE (neutered = true) AND (escape_attempts<'3');
+SELECT name FROM animals WHERE (neutered = true) AND (escape_attempts<'3');
 
-SELECT date_of_birth
-FROM animals
-WHERE (name = 'Agumon') OR (name = 'Pikachu');
+SELECT date_of_birth FROM animals WHERE (name = 'Agumon') OR (name = 'Pikachu');
 
-SELECT name, escape_attempts
-FROM animals
-WHERE weight_kg > '10.5';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > '10.5';
 
-SELECT *
-FROM animals
-WHERE neutered = true;
+SELECT * FROM animals WHERE neutered = true;
 
-SELECT *
-FROM animals
-WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE name <> 'Gabumon';
 
-SELECT *
-FROM animals
-WHERE (weight_kg >= '10.4') AND (weight_kg <= '17.3');
+SELECT * FROM animals WHERE (weight_kg >= '10.4') AND (weight_kg <= '17.3');
 
 -- update queries with transaction
 
 BEGIN;
 
-UPDATE animals
-SET species = 'unspecified';
+UPDATE animals SET species = 'unspecified';
 
 ROLLBACK;
 
 BEGIN;
 
-UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon';
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 
-UPDATE animals
-SET species = 'pokemon'
-WHERE species IS NULL;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 
 COMMIT;
 
@@ -61,55 +40,34 @@ ROLLBACK;
 
 BEGIN;
 
-DELETE FROM animals
-WHERE date_of_birth > '2022-01-01';
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 
 SAVEPOINT DOB;
 
-UPDATE animals
-SET weight_kg = weight_kg *-1;
+UPDATE animals SET weight_kg = weight_kg *-1;
 
 ROLLBACK TO SAVEPOINT DOB;
 
-UPDATE animals
-SET weight_kg = weight_kg*-1
-WHERE weight_kg <0;
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg <0;
 
 COMMIT;
 
 -- aggregate functions
 
-SELECT COUNT(*)
-FROM animals;
+SELECT COUNT(*) FROM animals;
 
-SELECT COUNT(*)
-FROM animals
-WHERE escape_attempts = '0';
+SELECT COUNT(*) FROM animals WHERE escape_attempts = '0';
 
-SELECT AVG(weight_kg)
-FROM animals;
+SELECT AVG(weight_kg) FROM animals;
 
-SELECT neutered, MAX(escape_attempts)
-FROM animals
-GROUP BY neutered
-ORDER BY neutered
-DESC LIMIT 1;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered ORDER BY neutered DESC LIMIT 1;
 
-SELECT
-    species,
-    MIN(weight_kg),
-    MAX(weight_kg)
-FROM animals
-GROUP BY species;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
-SELECT species, AVG(escape_attempts)
-FROM animals
-GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals GROUP BY species;
 
-SELECT
-    species,
-    AVG(escape_attempts)
-FROM animals
-WHERE date_of_birth
-BETWEEN '1990-01-01' AND '2000-12-31'
-GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+-- JOIN queries
+
+SELECT name FROM animals A JOIN owners O ON O.id = A.owner_id WHERE O.id = 4;
--- schema.sql
+++ schema.sql
@@ -24,7 +24,6 @@ CREATE TABLE species(
 );
 
 ALTER TABLE animals ADD PRIMARY KEY(id),
-  DROP COLUMN species;
-
-ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
-ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+  DROP COLUMN species,
+  ADD COLUMN species_id INT REFERENCES species(id),
+  ADD COLUMN owner_id INT REFERENCES owners(id);
