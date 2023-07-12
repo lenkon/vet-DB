7315670f4ac55f4173318661f48964e0fa7c3112
Dilsher <dilsherb@gmail.com>
2023-07-12 12:30:57 +0300

    CR changes about weight are applied.

--- data.sql
+++ data.sql
@@ -22,10 +22,10 @@ INSERT INTO
     weight_kg
   )
 VALUES
-  ('Charmander', '2020-02-08', 0, false, 11),
-  ('Plantmon', '2021-11-15', 2, TRUE, 5.7),
-  ('Squirtle', '1993-04-02', 3, false, 12.13),
-  ('Angemon', '2005-06-12', 1, TRUE, 45),
+  ('Charmander', '2020-02-08', 0, false, -11),
+  ('Plantmon', '2021-11-15', 2, TRUE, -5.7),
+  ('Squirtle', '1993-04-02', 3, false, -12.13),
+  ('Angemon', '2005-06-12', 1, TRUE, -45),
   ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
   ('Blossom', '1998-10-13', 3, TRUE, 17),
   ('Ditto', '2022-05-14', 4, TRUE, 22);
