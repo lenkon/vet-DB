2d6d4f7715f042a1ee376d62b3310cc875c62ec0
Dilsher Balouch <dilsherb@gmail.com>
2023-07-13 21:35:20 +0300

    How many visits were with a vet that did not specialize in that animal's species?

--- queries.sql
+++ queries.sql
@@ -150,3 +150,31 @@ FROM visits Vi
 JOIN vets V ON v.id = Vi.vet_id
 JOIN animals A ON A.id = Vi.animal_id
 ORDER BY Vi.date_of_visit DESC LIMIT 1;
+
+-- three different queries to achieve the following
+-- "How many visits were with a vet that did not specialize in that animal's species?"
+-- first
+SELECT V.name, COUNT(V.name) FROM visits Vi
+FULL JOIN vets V ON v.id = Vi.vet_id
+FULL JOIN specializations SP ON SP.vet_id = v.id
+WHERE SP.vet_id IS NULL
+GROUP BY V.name;
+-- second
+SELECT vet_name,
+       COUNT(*) AS num_visits
+FROM visits vi
+JOIN vets v ON v.id = vi.vet_id
+JOIN specializations sp ON sp.vet_id = v.id
+WHERE sp.vet_id IS NULL
+GROUP BY v.name;
+-- third
+SELECT vet_name,
+       COUNT(vet_name) AS num_visits
+FROM (
+    SELECT v.name AS vet_name
+    FROM visits vi
+    FULL JOIN vets v ON v.id = vi.vet_id
+    FULL JOIN specializations sp ON sp.vet_id = v.id
+    WHERE sp.vet_id IS NULL
+) AS t
+GROUP BY vet_name;
