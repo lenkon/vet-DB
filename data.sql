3148f1757e4a7c1c6af91f7328d55fa867f6fbc1
Dilsher <dilsherb@gmail.com>
2023-07-17 14:07:16 +0300

    some more data inserted into owners table.

--- data.sql
+++ data.sql
@@ -105,8 +105,14 @@ VALUES
     (10, 3, '2020-05-24'),
     (10, 1, '2021-01-11');
 
-    INSERT INTO
-        visits (animal_id, vet_id, date_of_visit)
-        SELECT * FROM (SELECT id FROM animals) animal_ids,
-        (SELECT id FROM vets) vets_ids,
-        generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+INSERT INTO
+    visits (animal_id, vet_id, date_of_visit)
+    SELECT * FROM (SELECT id FROM animals) animal_ids,
+    (SELECT id FROM vets) vets_ids,
+    generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into
+    owners (full_name, email)
+    select 'Owner ' ||
+    generate_series(1,2500000), 'owner_' ||
+    generate_series(1,2500000) || '@mail.com';
