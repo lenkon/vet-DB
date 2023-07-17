8f9bef62dfe7077e10c2e1495cd972c22cfc033a
lenkon <inoulenkon@gmail.com>
2023-07-17 18:46:12 +0530

    Add alternative performance check

--- queries.sql
+++ queries.sql
@@ -164,3 +164,8 @@ SELECT COUNT(*) FROM visits where animal_id = 4;
 SELECT * FROM visits where vet_id = 2;
 
 SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+SELECT COUNT(animal_id)
+FROM visits
+WHERE animal_id = 4
+LIMIT 1;
